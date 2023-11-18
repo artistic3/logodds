3f5420022a8833ee01208d45bb67b4f6ba765dd5
artistic3 <87168661+artistic3@users.noreply.github.com>
2023-11-18 17:02:23 +0800

    Update sortodds.php

--- sortodds.php
+++ sortodds.php
@@ -71,6 +71,7 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
     
     $racetext .= "\t\t'All Runners   '  =>  '" . implode(", ", $runners).  "',\n";
     $racetext .= "\t\t'place' => '3 numbers: 12 and those with odds right lower and bigger than number 12's odds',\n";
+    $racetext .= "\t\t'place' => 'if 12 is last in odds then favorite win',\n";
 
     $racetext .= "\t],\n";
     if(!$showRace) $outtext .= $racetext;
