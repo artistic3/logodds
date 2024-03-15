fc0d5e9346bafca6c093f74e0bc8c18cf9bdec77
artistic3 <artisticsoftware3@gmail.com>
2024-03-15 11:53:27 +0000

    ...

--- 0316/bets.php
+++ 0316/bets.php
@@ -13,9 +13,11 @@ return [
 		/**
 		Race 2
 		*/
-		'favorites' => '9',
+		'favorites' => '9, 4',
 		'Fav 9' => '2, 3, 6, 9',
-		'union' => '2, 3, 6, 9',//count: 4
+		'Fav 4' => '3, 4, 5, 7, 8, 12',
+		'union' => '2, 3, 4, 5, 6, 7, 8, 9, 12',//count: 9
+		'win/qin' => '9, 4',
 	],
 	'3' => [
 		/**
@@ -40,51 +42,64 @@ return [
 		Race 5
 		*/
 		'favorites' => '10, 4',
+		'additional favorites' => '8',
 		'Fav 10' => '2, 4, 11',
 		'Fav 4' => '1, 2, 3, 4, 6, 7',
 		'union' => '1, 2, 3, 4, 6, 7, 11',//count: 7
-		'win/qin' => '10, 4',
+		'win/qin' => '10, 4, 8',
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
 		'favorites' => '12, 2, 6',
+		'additional favorites' => '8',
 		'Fav 12' => '1, 11, 12',
 		'Fav 2' => '2, 3, 4, 5, 8, 10',
+		'Fav 8' => '3, 8, 12',
 		'union' => '1, 2, 3, 4, 5, 8, 10, 11, 12',//count: 9
-		'win/qin' => '12, 2, 6',
+		'win/qin' => '12, 2, 6, 8',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
 		'favorites' => '6, 4',
+		'additional favorites' => '8',
 		'Fav 6' => '6, 10',
-		'union' => '6, 10',//count: 2
+		'Fav 8' => '2, 8, 9',
+		'union' => '2, 6, 8, 9, 10',//count: 5
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
-		'favorites' => '7',
+		'favorites' => '7, 1',
+		'additional favorites' => '8',
 		'Fav 7' => '1, 2, 3, 4, 5',
-		'union' => '1, 2, 3, 4, 5',//count: 5
+		'Fav 1' => '1, 5, 6, 7, 11',
+		'union' => '1, 2, 3, 4, 5, 6, 7, 11',//count: 8
+		'win/qin' => '7, 1, 8',
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
 		'favorites' => '5',
+		'additional favorites' => '8',
 		'Fav 5' => '3, 4, 5, 8, 11, 12',
-		'union' => '3, 4, 5, 8, 11, 12',//count: 6
+		'Fav 8' => '1, 5, 8',
+		'union' => '1, 3, 4, 5, 8, 11, 12',//count: 7
+		'win/qin' => '5, 8',
 	],
 	'10' => [
 		/**
 		Race 10
 		*/
 		'favorites' => '9, 7, 14',
+		'additional favorites' => '8',
 		'Fav 7' => '7, 8, 11, 12',
+		'Fav 8' => '7, 8',
 		'union' => '7, 8, 11, 12',//count: 4
 	],
 ];
