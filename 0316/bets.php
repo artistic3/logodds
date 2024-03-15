74fa31eace881ec93ebf743c1854666f271688c6
artistic3 <artisticsoftware3@gmail.com>
2024-03-15 06:05:30 +0000

    ...

---
+++ 0316/bets.php
@@ -0,0 +1,72 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '3',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '9',
+		'Fav 9' => '2, 3, 6, 9',//count: 4
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '3',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '11',
+		'Fav 11' => '6, 11, 12',//count: 3
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '10',
+		'Fav 10' => '2, 4, 11',//count: 3
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '12',
+		'Fav 12' => '1, 11, 12',//count: 3
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '6',
+		'Fav 6' => '6, 10',//count: 2
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '7',
+		'Fav 7' => '1, 2, 3, 4, 5',//count: 5
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '5',
+		'Fav 5' => '3, 4, 5, 8, 11, 12',//count: 6
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '9',
+		'Fav 9' => '11',//count: 1
+	],
+];
