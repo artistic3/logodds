b467d42f0213e4d8cb628af349092b6a1c4fac5b
artistic3 <87168661+artistic3@users.noreply.github.com>
2024-03-20 13:04:49 +0800

    Update bets.php

--- bets.php
+++ bets.php
@@ -57,7 +57,7 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
     foreach($favorites as $one){
         if(isset($history[$raceNumber][$one]['win'])){
             $winners = $history[$raceNumber][$one]['win'];
-            if(count($winners) > 7 || count($winners) < 3) continue;
+            if(count($winners) > 6 || count($winners) < 3) continue;
             $sets[$one] = $winners;
         } 
     }
