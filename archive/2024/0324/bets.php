3b527974b0499661bca41d4a06ec8bcd282b1fb2
artistic3 <artisticsoftware3@gmail.com>
2024-03-26 12:33:09 +0000

    ...

--- archive/2024/0324/bets.php
+++ archive/2024/0324/bets.php
@@ -8,7 +8,6 @@ return [
 		'favorites' => '5',
 		'win hist(Fav 5)' => '1, 5, 7, 8, 9, 12',//count: 6
 		'win' =>   '1, 5, 7, 8, 9, 12',//count: 6
-		'inter' =>   '1, 5, 7, 8, 9, 12',
 	],
 	'2' => [
 		/**
@@ -17,7 +16,7 @@ return [
 		'favorites' => '2, 1, 14',
 		'win hist(Fav 2)' => '2, 4, 5, 6, 7, 8, 9, 12, 13',//count: 9
 		'inter' =>   '2, 4, 5, 6, 7, 8, 9, 12, 13',
-		'SURE WIN' => '1, 2, 14',
+		'SURE WIN(&qin)' => '1, 2, 14',
 	],
 	'3' => [
 		/**
@@ -26,7 +25,6 @@ return [
 		'favorites' => '6',
 		'win hist(Fav 6)' => '1, 3, 4, 5, 6, 12',//count: 6
 		'win' =>   '1, 3, 4, 5, 6, 12',//count: 6
-		'inter' =>   '1, 3, 4, 5, 6, 12',
 	],
 	'4' => [
 		/**
@@ -43,7 +41,7 @@ return [
 		'win hist(Fav 2)' => '1, 2, 7, 8, 9',//count: 5
 		'win hist(Fav 10)' => '1, 2, 3, 4, 11',//count: 5
 		'inter' =>   '1, 2',
-		'SURE WIN' => '1, 2, 10',
+		'SURE WIN(&qin)' => '1, 2, 10',
 	],
 	'6' => [
 		/**
@@ -52,7 +50,6 @@ return [
 		'favorites' => '1, 12',
 		'win hist(Fav 1)' => '1, 3, 7, 9, 12, 13',//count: 6
 		'win' =>   '1, 3, 7, 9, 12, 13',//count: 6
-		'inter' =>   '1, 3, 7, 9, 12, 13',
 	],
 	'7' => [
 		/**
@@ -61,8 +58,7 @@ return [
 		'favorites' => '7, 2, 5',
 		'win hist(Fav 2)' => '1, 2, 3, 4, 5, 6, 7, 9',//count: 8
 		'win' =>   '1, 2, 3, 4, 5, 6, 7, 9',//count: 8
-		'inter' =>   '1, 2, 3, 4, 5, 6, 7, 9',
-		'SURE WIN' => '2, 5, 7',
+		'SURE WIN(&qin)' => '2, 5, 7',
 	],
 	'8' => [
 		/**
@@ -80,7 +76,7 @@ return [
 		'win hist(Fav 2)' => '1, 2, 3, 4, 5, 6, 11',//count: 7
 		'win hist(Fav 11)' => '1, 3, 5, 9, 12',//count: 5
 		'inter' =>   '1, 5',
-		'SURE WIN' => '1, 2, 4, 11',
+		'SURE WIN(&qin)' => '1, 2, 4, 11',
 	],
 	'10' => [
 		/**
@@ -89,7 +85,6 @@ return [
 		'favorites' => '2, 8',
 		'win hist(Fav 2)' => '2, 3, 8, 13',//count: 4
 		'win' =>   '2, 3, 8, 13',//count: 4
-		'inter' =>   '2, 3, 8, 13',
-		'SURE WIN' => '2, 8',
+		'SURE WIN(&qin)' => '2, 8',
 	],
 ];
