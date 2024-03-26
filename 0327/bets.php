89cbf4fa6e9ce72b4fec2d526010879ed6bc7e48
artistic3 <artisticsoftware3@gmail.com>
2024-03-26 05:41:15 +0000

    ...

---
+++ 0327/bets.php
@@ -0,0 +1,63 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '6',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '5',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '7',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '8',
+		'win hist(Fav 8)' => '1, 3, 8, 11',//count: 4
+		'fibo seq(Fav 8)' => '1, 3, 8',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '3',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '5',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '6',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '1',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '11',
+		'win hist(Fav 11)' => '1, 3, 5, 9, 12',//count: 5
+		'fibo seq(Fav 11)' => '1, 3, 5',
+	],
+		//'shit' => '8, 11',
+];
