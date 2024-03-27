4d4af2865d4543a859c59f3f4d5f779e9c93e46a
artistic3 <artisticsoftware3@gmail.com>
2024-03-27 12:43:24 +0000

    ...

--- 0327/bets.php
+++ 0327/bets.php
@@ -38,9 +38,10 @@ return [
 		/**
 		Race 5
 		*/
-		'favorites' => '3, 1',
+		'favorites' => '3, 1, 4',
 		'win hist(Fav 1)' => '1, 2, 3, 4, 7, 10, 12',//count: 7
-		'win' =>   '1, 2, 3, 4, 7, 10, 12',//count: 7
+		'win hist(Fav 4)' => '1, 2, 3, 4, 6, 7',//count: 6
+		'win' =>   '1, 2, 3, 4, 6, 7, 10, 12',//count: 8
 	],
 	'6' => [
 		/**
